32f8a593865d361ea85bf18f7a1601d46277b260
Tom <tomasvergara272@gmail.com>
2023-09-28 00:45:18 -0300

    primer anidado

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -17,6 +19,11 @@ export class SelectorPageComponent {
 
   constructor(
     private fb: FormBuilder,
+    private countriesService: CountriesService,
   ){}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 
 }
---
+++ src/app/country/services/countries.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private _regions: Region[] = [ Region.Africa, Region.America, Region.Asia, Region.Europa, Region.Oceania ];
+
+  constructor() { }
+
+  get regions(): Region[] {
+    return [ ...this._regions ];
+  }
+}
